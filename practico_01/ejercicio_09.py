fd710d4c19128be56d2eab1184ec1ac818b18396
Luca Gini <64549503+LucaGini@users.noreply.github.com>
2024-04-27 10:57:23 -0300

    Update ejercicio_09.py
    Ejercicio 9 corregido

--- practico_01/ejercicio_09.py
+++ practico_01/ejercicio_09.py
@@ -24,7 +24,7 @@ Referencia: https://docs.python.org/3/library/functions.html#sum
 """
 
 def sumatoria_sum(n: int) -> int:
-    suma = sum(range(0, n))
+    return sum(range(1, n+1))
 
     pass # Completar
 
@@ -45,9 +45,10 @@ Referencia: https://docs.python.org/3/library/functools.html#functools.reduce
 """
 
 def sumatoria_reduce(n: int) -> int:
-    suma = reduce(lambda x, y: x + y, range(0,n))
+    suma = reduce(lambda x, y: x + y, range(1,n+1))
     return suma
-
+#utilizamos range(1, n + 1) para generar una secuencia de números desde 1 hasta N
+# (ambos inclusive), y luego aplicamos la función reduce para obtener la suma total.
 
     pass # Completar
 
@@ -66,6 +67,7 @@ def sumatoria_gauss(n: int) -> int:
     """CHALLENGE OPCIONAL: Re-Escribir utilizando suma de Gauss.
     Referencia: https://es.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
     """
+    return n*(n+1)//2 #formula de la suma de gauss, buscada en internet
     pass # Completar
 
 
